3616339b0c0d21b0ab03c35c2e87bfb2c5703002
izhatomic <izhatomic@yandex.ru>
2021-12-11 19:35:00 +0300

    Сделал ansible роли

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,14 +14,23 @@ provider "yandex" {
   zone      = "ru-central1-c"
 }
 
-resource "yandex_compute_instance" "default" {
-  name        = "test"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-c"
+resource "yandex_vpc_network" "network_1" {
+  name = "network_1"
+}
+
+resource "yandex_vpc_subnet" "subnet_1" {
+  name           = "subnet_1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network_1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_compute_instance" "vm1" {
+  name = "terraform1"
 
   resources {
-    cores         = 2
-    memory        = 2
+    cores  = 2
+    memory = 2
     core_fraction = 20
   }
 
@@ -32,13 +41,27 @@ resource "yandex_compute_instance" "default" {
   }
 
   network_interface {
-    nat       = "true"
-    subnet_id = "b0c3eh2qaofainqtrg4o"
+#    subnet_id = yandex_vpc_subnet.subnet_1.id
+    subnet_id = "e9bqstrd9l7dpc6g3l3e"
+#    subnet_id = "b0c3eh2qaofainqtrg4o"
+    nat       = true
   }
 
   metadata = {
-    #    foo = "bar"
-    ssh-keys  = "izhatomic:${file("~/.ssh/id_rsa.pub")}"
-#    user-data = "${file("/home/izhatomic/GIT/NG/users.txt")}"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    user-data = "${file("users.txt")}"
   }
+}
+
+
+output "yandex_vpc_subnet_subnet_1_id" {
+  value = yandex_vpc_subnet.subnet_1.id
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm1.network_interface.0.nat_ip_address
 }
